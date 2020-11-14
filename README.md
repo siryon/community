874a11bee2d62b39c4ce108929e48b116e3e5740
siryon <15622796766@163.com>
2020-11-14 18:17:07 +0800

    add index and bootstrap

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 ##  资料
 https://spring.io/guides
 https://www.bilibili.com/video/BV1r4411r7au?p=4
-
+https://docs.github.com/en/free-pro-team@latest/developers/apps/creating-an-oauth-app
 ##  工具
 https://github.com/
--- src/main/java/community2/demo/controller/HelloController.java
+++ src/main/java/community2/demo/controller/HelloController.java
@@ -3,14 +3,18 @@ package community2.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
+
+    @GetMapping("/")
+    public String login(){
+        return "index";
+    }
+
     @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name);
+    public String hello(){
         return "hello";
     }
 }
